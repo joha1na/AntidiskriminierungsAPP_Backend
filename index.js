a4bf769e12b6c8e73f5a55c6114136f311c2f781
Jana Maire <s0563285@htw-berlin.de>
2024-02-27 21:44:12 +0100

    Durchreichen Sprache und Anspprechpartnerinformationen im Kontaktformular zur Mail; Formatieren Mail und Anzeige in Abhängigkeit von Formulartyp und Kontaktangaben

--- index.js
+++ index.js
@@ -18,8 +18,13 @@ app.post('/send-email', (req, res) => {
     const lastname = req.body.lastname;
     const firstname = req.body.firstname;
     const email = req.body.email;
-    const checkbox = req.body.checkbox;
+    var checkbox = req.body.checkbox;
     const formulartyp = req.body.formulartyp;
+    const apmail = req.body.apmail;
+    const titel = req.body.titel;
+    const vorname = req.body.vorname;
+    const nachname = req.body.nachname;
+    var sprache = req.body.sprache;
 
     const transporter = nodemailer.createTransport({
         host: process.env.host,
@@ -30,22 +35,82 @@ app.post('/send-email', (req, res) => {
         }
     });
 
-    const mailOptions = {
-        from: process.env.from,
-        to: process.env.to,
-        subject: `Neue Nachricht über das ${formulartyp} der AntidiskriminierungsAPP`,
-        text: `
-        Folgende Informationen wurden in das ${formulartyp} eingegeben:
-        Mitgliedergruppe: ${mitgliedergruppe}
-        Betroffenheit: ${betroffenheit}
-        Nachricht: ${message}
-        Kategorie: ${category}
-        Nachname: ${lastname}
-        Vorname: ${firstname}
-        E-Mail: ${email}
-        Zustimmung zur Datenverarbeitung: ${checkbox}
-    `
-    };
+    var mailOptions = {};
+
+    if (checkbox) {
+        checkbox = 'ja';
+    }
+
+    if (sprache == 'de') {
+        sprache = "deutsch";
+    } else if (sprache == 'en') {
+        sprache = "englisch";
+    }
+
+    if (formulartyp == 'Kontaktformular' && nachname !== 'NA') {
+        mailOptions = {
+            from: process.env.from,
+            to: process.env.to,
+            subject: `Neue Nachricht an ${apmail} über das ${formulartyp} der AntidiskriminierungsAPP`,
+            text: `
+            Folgende Informationen wurden in das ${formulartyp} eingegeben:
+            Mitgliedergruppe: ${mitgliedergruppe}
+            Betroffenheit: ${betroffenheit}
+            Nachricht: ${message}
+            Kategorie: ${category}
+            Nachname: ${lastname}
+            Vorname: ${firstname}
+            E-Mail: ${email}
+            Zustimmung zur Datenverarbeitung: ${checkbox}
+
+            Folgende Ansprechperson wurde ${formulartyp} ausgewählt:
+            Titel: ${titel}
+            Vorname: ${vorname}
+            Nachname: ${nachname}
+
+            Die App wurde auf ${sprache} genutzt.
+        `
+        };
+    } else if (formulartyp == 'Kontaktformular' && nachname == 'NA') {
+        mailOptions = {
+            from: process.env.from,
+            to: process.env.to,
+            subject: `Neue Nachricht an ${apmail} über das ${formulartyp} der AntidiskriminierungsAPP`,
+            text: `
+            Folgende Informationen wurden in das ${formulartyp} eingegeben:
+            Mitgliedergruppe: ${mitgliedergruppe}
+            Betroffenheit: ${betroffenheit}
+            Nachricht: ${message}
+            Kategorie: ${category}
+            Nachname: ${lastname}
+            Vorname: ${firstname}
+            E-Mail: ${email}
+            Zustimmung zur Datenverarbeitung: ${checkbox}
+
+            Im ${formulartyp} wurde keine Anspechperson ausgewählt.
+
+            Die App wurde auf ${sprache} genutzt.
+        `
+        };
+    } else if (formulartyp == 'Meldeformular') {
+        mailOptions = {
+            from: process.env.from,
+            to: process.env.to,
+            subject: `Neue Nachricht über das ${formulartyp} der AntidiskriminierungsAPP`,
+            text: `
+            Folgende Informationen wurden in das ${formulartyp} eingegeben:
+            Mitgliedergruppe: ${mitgliedergruppe}
+            Nachricht: ${message}
+            Kategorie: ${category}
+            Nachname: ${lastname}
+            Vorname: ${firstname}
+            E-Mail: ${email}
+            Zustimmung zur Datenverarbeitung: ${checkbox}
+            
+            Die App wurde auf ${sprache} genutzt.
+        `
+        };
+    }
 
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
