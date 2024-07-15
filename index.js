69cf002cb5218ca0a31dc1e016ca0a4049c3b766
Prof. Dr.-Ing. Jörn Freiheit <joern.freiheit@htw-berlin.de>
2024-07-15 14:17:09 +0200

    fuer server antidis.f4.htw-berlin.de

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.serverport;
 
+console.log('from', process.env.from)
+console.log('to', process.env.to)
+
 app.use(bodyParser.json());
 app.use(cors());
 
